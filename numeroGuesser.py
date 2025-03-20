225109d5a25b37a4e7212f9bf58e9713a06999db
Gerald Sadya <sadyageraldm@gmail.com>
2025-03-20 20:06:45 +0200

    Initial commit - defining the project attributes

---
+++ numeroGuesser.py
@@ -0,0 +1,17 @@
+import random
+# Number Guessing Game
+
+def welcome():
+    print("Welcome to the number guessing game.")
+    print("I'm thinking of a number between 1 and 100.")
+    print("You have 5 chances to guess the correct number.")
+
+def difficulty():
+    print("Please select the difficulty level:")
+    print("1. Easy (10 chances)")
+    print("2. Medium (5 chances)")
+    print("3. Hard (3 chances)")
+
+
+
+def main():
