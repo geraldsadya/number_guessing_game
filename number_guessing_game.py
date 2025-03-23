5ef81ad34bf2f8a475834f73572a5530b0817fc3
Gerald Sadya <sadyageraldm@gmail.com>
2025-03-24 00:53:25 +0200

    Defining the project and coming up with new function definitions

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,17 +1,28 @@
 import random
 # Number Guessing Game
 
-def welcome():
+def display_welcome():
     print("Welcome to the number guessing game.")
     print("I'm thinking of a number between 1 and 100.")
     print("You have 5 chances to guess the correct number.")
 
-def difficulty():
+def select_difficulty():
     print("Please select the difficulty level:")
     print("1. Easy (10 chances)")
     print("2. Medium (5 chances)")
     print("3. Hard (3 chances)")
 
 
+def generate_random_number():
 
-def main():
+def play_game():
+
+def check_guess(guess, target):
+    if guess == target:
+        print("Congratulations! You guessed the correct number.")
+        return True
+    else:
+        print("Sorry, you guessed the wrong number.")
+        return False
+
+def play_again():
